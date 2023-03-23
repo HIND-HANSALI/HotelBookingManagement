8299cd8ba22e9bd91d72bedd9ba8b70b1987c8a1
HIND-HANSALI <hindhsl111@gmail.com>
2023-03-23 17:17:06 +0000

    route get facilities view

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/facilities', function () {
+    return view('facilities');
+})->name('facilities');
