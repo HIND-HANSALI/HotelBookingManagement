9b4aa6f781557e91cb2e9819e13bdbffb1b86bd0
HIND-HANSALI <hindhsl111@gmail.com>
2023-03-23 14:00:55 +0000

    add Reach Us Section

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -431,7 +431,48 @@
         </div>
     </section>
 
-   
+    <!-- Reach us-->
+
+    <h2 class="mt-5 pt-4 mb-4 text-center fw-bold h-font">Reach Us</h2>
+
+
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8 col-md-8 p-4 mb-lg-0 mb-3 bg-white rounded">
+                <iframe class="w-100 rounded" height="320px" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3433.0450626310585!2d-6.463368571340776!3d32.10828139295283!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xda4817b2a443c81%3A0x5c5787cfb96eef7e!2sBin%20El-Ouidane!5e0!3m2!1sfr!2sma!4v1679409307067!5m2!1sfr!2sma" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+            <div class="col-lg-4 col-md-4 ">
+                <div class="bg-white p-4 rounded">
+                    <h5>Call us</h5>
+                    <a href="tel: +212 656457890" class="d-inline-block mb-2 text-decoration-none text-dark"><i class="bi bi-telephone-fill"></i> +212 656457890</a>
+                    <br>
+                    <a href="tel: +212 656457890" class="d-inline-block mb-2 text-decoration-none text-dark"><i class="bi bi-telephone-fill"></i> +212 656457890</a>
+                </div>
+                <div class="bg-white p-4 rounded">
+                    <h5>Follow us</h5>
+                    <a href="#" class="d-inline-block mb-3">
+                        <span class="badge bg-light text-dark fs-6 p-2">
+                            <i class="bi bi-twitter me-1"></i>Twitter
+                        </span>
+                    </a>
+                    <br>
+                    <a href="#" class="d-inline-block mb-3">
+                        <span class="badge bg-light text-dark fs-6 p-2">
+                            <i class="bi bi-facebook me-1"></i>Facebook
+                        </span>
+                    </a>
+                    <br>
+                    <a href="#" class="d-inline-block">
+                        <span class="badge bg-light text-dark fs-6 p-2">
+                            <i class="bi bi-instagram me-1"></i>Instagram
+                        </span>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <hr>
+
     <!-- Swiper JS -->
     <script src="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.js"></script>
 
