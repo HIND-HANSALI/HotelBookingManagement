7c1902ef68692d6339c97360b8e68d0397cbaef6
HIND-HANSALI <hindhsl111@gmail.com>
2023-03-24 23:01:13 +0000

    definir les routes facilities

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\FacilitieController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,9 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/facilities', function () {
-    return view('facilities');
-})->name('facilities');
+// Route::get('/facilities', function () {
+//     return view('facilities');
+// })->name('facilities');
 
 Route::get('/about', function () {
     return view('about');
@@ -31,3 +31,20 @@ Route::get('/contact', function () {
 Route::get('/rooms', function () {
     return view('rooms');
 })->name('rooms');
+
+    Route::view('/dashboard','dashboard.index');
+    // Route::view('/all-facilities','dashboard.all-facilities');
+// Route::get('/facilities', function () {
+//         return view('dashboard.all-facilities');
+//     })->name('facilities');
+    Route::get('/addfacilitie', function () {
+        return view('dashboard.add-facilitie');
+    })->name('addfacilitie');
+    Route::get('/editfacilitie', function () {
+        return view('dashboard.edit-facilitie');
+    })->name('editfacilitie');
+
+    Route::get('/facilities', [FacilitieController::class,'index'])->name('facilities');
+    Route::resource('facilitiess', FacilitieController::class)->only(['index','show','create','store','edit','update','destroy']);
+    // Route::get('facilitiess/{facilitiess}',[FacilitieController::class,'edit']);
+    // Route::post('facilitiess', [FacilitieController::class, 'store'])->name('facilities.store');
