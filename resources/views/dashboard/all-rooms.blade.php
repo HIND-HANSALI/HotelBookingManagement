fd5c62f35e5eac246b1714a31135afaa098d4c43
HIND-HANSALI <hindhsl111@gmail.com>
2023-03-26 18:18:14 +0000

    add Create Image Room page

--- resources/views/dashboard/all-rooms.blade.php
+++ resources/views/dashboard/all-rooms.blade.php
@@ -154,7 +154,11 @@
 												</td>
 												<td class="text-right">
 													<div class="dropdown dropdown-action"> <a href="#" class="action-icon dropdown-toggle" data-toggle="dropdown" aria-expanded="false"><i class="fas fa-ellipsis-v ellipse_color"></i></a>
-														<div class="dropdown-menu dropdown-menu-right"> <a class="dropdown-item" href="edit-room.html"><i class="fas fa-pencil-alt m-r-5"></i> Edit</a> <a class="dropdown-item" href="#" data-toggle="modal" data-target="#delete_asset"><i class="fas fa-trash-alt m-r-5"></i> Delete</a> </div>
+														<div class="dropdown-menu dropdown-menu-right"> 
+														<a class="dropdown-item" href="edit-room.html"><i class="fas fa-pencil-alt m-r-5"></i> Edit</a> 
+														<a class="dropdown-item" href="#" data-toggle="modal" data-target="#delete_asset"><i class="fas fa-trash-alt m-r-5"></i> Delete</a> 
+														<a class="dropdown-item" href="{{ route('roomImage') }}"><i class="fas fa-image m-r-5"></i>Add Image</a>
+													</div>
 													</div>
 												</td>
 											</tr>
