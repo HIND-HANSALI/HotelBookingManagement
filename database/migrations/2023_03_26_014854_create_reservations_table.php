bc2be656a14748ec6e9d4f51e14cfe5619a78c89
HIND-HANSALI <hindhsl111@gmail.com>
2023-03-26 04:34:57 +0000

    create Booking migration

---
+++ database/migrations/2023_03_26_014854_create_reservations_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reservations', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->date('checkIn');
+            $table->date('checkOut');
+            $table->string('statutBooking');
+            $table->string('typeBooking');
+            $table->float('totalPrice');
+            $table->integer('numberPerson');
+            $table->integer('room_id');
+            $table->integer('user_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reservations');
+    }
+};
