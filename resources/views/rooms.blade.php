edb0946d793933f0af31a2e15513c3c0ecf9c946
HIND-HANSALI <hindhsl111@gmail.com>
2023-03-30 12:28:48 +0000

    modify rooms blade

--- resources/views/rooms.blade.php
+++ resources/views/rooms.blade.php
@@ -74,7 +74,7 @@
                                     <label class="form-check-label" for="f3">AC</label>
                                 </div>
                             </div>
-                            <div class="border bg-light p-3 rounded mb-3">
+                            <!-- <div class="border bg-light p-3 rounded mb-3">
                                 <h5 class="mb-3" style="font-size: 18px;">Adults</h5>
                                 <div class="d-flex">
                                     <div class="me-2">
@@ -86,7 +86,7 @@
                                         <input type="number" class="form-control shadow-none">
                                     </div>
                                 </div>
-                            </div>
+                            </div> -->
                         </div>
                     </div>
                 </nav>
@@ -129,17 +129,48 @@
                         <div class="col-md-2 mt-lg-0 mt-md-0 mt-4 text-center">
                             <h6 class="mb-4">50 MAD per night </h6>
                             <a href="#" class="btn btn-sm w-100 text-white custom-bg shadow-none mb-2">Book Now</a>
-                            <a href="{{ route('roomdetails') }}" class="btn btn-sm w-100 btn-outline-dark shadow-none">More details</a>
+                            <a href="{{ route('roomdetails') }}" class="btn btn-sm w-100 btn-outline-dark shadow-none">Check details</a>
                         </div>
                     </div>
                 </div>
                 <div class="card mb-4 border-0 shadow">
                     <div class="row g-0 p-3 align-items-center">
                         <div class="col-md-5 mb-lg-0 mb-md-0 mb-3">
-                            <img src="https://nomadsworld.com/wp-content/uploads/2018/11/nomads-brisbane-hostel-dorm.jpg" class="img-fluid rounded">
+                            <!-- <img src="https://nomadsworld.com/wp-content/uploads/2018/11/nomads-brisbane-hostel-dorm.jpg" class="img-fluid rounded"> -->
+                            <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+                                <div class="carousel-inner">
+                                    <div class="carousel-item active">
+                                        <img src="https://nomadsworld.com/wp-content/uploads/2018/11/nomads-brisbane-hostel-dorm.jpg" class="d-block w-100" alt="...">
+                                    </div>
+                                    <div class="carousel-item">
+                                        <img src="https://media.istockphoto.com/id/182498079/fr/photo/auberge-de-jeunesse-dortoir.jpg?s=612x612&w=0&k=20&c=HBSKWPnqg7ihEz-kRpFl60B4VW4odBq0j5Wl_RBs3LY=" class="d-block w-100" alt="...">
+                                    </div>
+                                    <div class="carousel-item">
+                                        <img src="https://media.istockphoto.com/id/182498079/fr/photo/auberge-de-jeunesse-dortoir.jpg?s=612x612&w=0&k=20&c=HBSKWPnqg7ihEz-kRpFl60B4VW4odBq0j5Wl_RBs3LY=" class="d-block w-100" alt="...">
+                                    </div>
+                                </div>
+                                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                    <span class="visually-hidden">Previous</span>
+                                </button>
+                                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                    <span class="visually-hidden">Next</span>
+                                </button>
+                            </div>
                         </div>
                         <div class="col-md-5 px-lg-3 px-md-3 px-0">
-                            <h5 class="mb-3"> Room Name</h5>
+
+                            <div class="d-flex align-items-center mb-2">
+                                <h5 class="mb-1 me-2">Room Paradise</h5>
+                                <div>
+                                    <i class="bi bi-star-fill text-warning"></i>
+                                    <i class="bi bi-star-fill text-warning"></i>
+                                    <i class="bi bi-star-fill text-warning"></i>
+                                    <i class="bi bi-star-fill text-warning"></i>
+                                </div>
+
+                            </div>
 
                             <div class="Facilities mb-3">
                                 <h6 class="mb-1">Facilities</h6>
@@ -164,10 +195,18 @@
 
                             </div>
                         </div>
-                        <div class="col-md-2 text-center">
-                            <h6 class="mb-4">50 MAD per night </h6>
+                        <div class="col-md-2 ">
+                            <a href="#!" class="text-dark ms-auto d-flex justify-content-end">
+                                <i class="bi bi-heart"></i>
+                            </a>
+
+                            <h6 class="mb-4 text-center">50 MAD per night </h6>
+                            <div class="d-flex justify-content-lg-between">
+                                <h6 class="text-striked text-muted mr-2">100 MAD</h6>
+                                <h6 class="text-success">32% off</h6>
+                            </div>
                             <a href="#" class="btn btn-sm w-100 text-white custom-bg shadow-none mb-2">Book Now</a>
-                            <a href="#" class="btn btn-sm w-100 btn-outline-dark shadow-none">More details</a>
+                            <a href="#" class="btn btn-sm w-100 btn-outline-dark shadow-none">Check details</a>
                         </div>
                     </div>
                 </div>
