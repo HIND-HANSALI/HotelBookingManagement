42e4deae4942ec90f4693a734e5c38ee4de636d3
HIND-HANSALI <hindhsl111@gmail.com>
2023-04-18 23:49:40 +0000

    add reviewratings migration

---
+++ database/migrations/2023_04_18_221050_create_review_ratings_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('review_ratings', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->unsignedBigInteger('chambre_id');
+            $table->string('name');
+            $table->string('email');
+            $table->string('phone');
+            $table->longText('comments')->nullable();
+            $table->integer('star_rating');
+            
+            $table->foreign('chambre_id')->references('id')->on('chambres')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('review_ratings');
+    }
+};
