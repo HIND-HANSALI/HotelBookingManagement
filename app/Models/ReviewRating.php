dcb23b56f42a170fa15f33fb837d1b41a898a47e
HIND-HANSALI <hindhsl111@gmail.com>
2023-04-20 14:42:54 +0000

    users can add review for a room

---
+++ app/Models/ReviewRating.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ReviewRating extends Model
+{
+    use HasFactory;
+    
+    public function chambre()
+    {
+        return $this->belongsTo(Chambre::class);
+    }
+}
